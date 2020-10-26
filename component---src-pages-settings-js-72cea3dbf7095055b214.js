2dc67e1607689e2438480c49f7c7dbbb0f43481c
domman95 <dominiklasek95@gmail.com>
2020-10-26 11:12:28 +0100

    Updates

---
+++ component---src-pages-settings-js-72cea3dbf7095055b214.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{pM26:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return o}));var a=e("q1tI"),i=e.n(a),s=e("vOnD"),c=e("tc9R"),l=s.b.div.withConfig({displayName:"settings__SettingsPageStyles",componentId:"sc-7fl0ff-0"})([""]);function o(){return i.a.createElement(l,null,i.a.createElement(c.a,null,"Ustawienia"))}}}]);
+//# sourceMappingURL=component---src-pages-settings-js-72cea3dbf7095055b214.js.map
